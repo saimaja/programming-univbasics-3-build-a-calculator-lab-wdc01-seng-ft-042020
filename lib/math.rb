6be1dc82949fc61c43c9205a487bd68690a05b2e
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-26 20:59:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,25 +5,25 @@ end
 
 def subtraction(num1, num2)
   result = num1 - num2
-  puts result
+  return result
 end
 
 def division(num1, num2)
   result = num1 / num2
-  puts result
+  return result
 end
 
 def multiplication(num1, num2)
   result = num1 * num2
-  puts result
+  return result
 end
 
 def modulo(num1, num2)
   result = num1 % num2
-  puts result
+  return result
 end
 
 def square_root(num)
   result = Math.sqrt(num)
-  puts result
+  return result
 end
