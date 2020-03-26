5f99ce5d4727353d2a041d27e7cf0f2518f8cc15
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-26 20:55:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
 def addition(num1, num2)
-
+  result = num1 + num2
+  puts result
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  puts result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  puts result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  puts result
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  puts result
 end
 
 def square_root(num)
